7cc8f71e46134d560e28466aea06303503b5fc77
LeeSeokHo <rrgks@naver.com>
2023-06-04 22:20:38 +0900

    reafactor: ConfigModule 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { apiModules } from '@src/apis';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { CoreModule } from '@src/core/core.module';
 import { LoggerMiddleware } from '@src/middlewares/logger.middleware';
-import Joi from 'joi';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-      validationSchema: Joi.object({
-        PORT: Joi.number().default(3000),
-        SECRET_KEY: Joi.string(),
-        DATABASE_URL: Joi.string(),
-      }),
-      isGlobal: true,
-    }),
-    CoreModule,
-    ...apiModules,
-  ],
+  imports: [CoreModule, ...apiModules],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/core/app-config/app-config.module.ts
+++ src/core/app-config/app-config.module.ts
@@ -1,8 +1,23 @@
 import { Global, Module, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_KEY } from '@src/core/app-config/constants/api-config.constant';
 import { AppConfigService } from '@src/core/app-config/services/app-config.service';
+import Joi from 'joi';
 
 @Global()
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', 'env'],
+      validationSchema: Joi.object({
+        [ENV_KEY.PORT]: Joi.number().default(3000),
+        [ENV_KEY.NODE_ENV]: Joi.string().required(),
+        [ENV_KEY.JWT_ACCESS_KEY]: Joi.string().required(),
+        [ENV_KEY.DATABASE_URL]: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+  ],
   providers: [AppConfigService],
   exports: [AppConfigService],
 })
