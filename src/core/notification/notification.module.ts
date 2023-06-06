295386eabe4c867c830fe95c72705fd17ef7a6cc
LeeSeokHo <rrgks@naver.com>
2023-06-06 17:14:51 +0900

    feat: create notification service

--- src/core/notification/notification.module.ts
+++ src/core/notification/notification.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { NotificationService } from '@src/core/notification/services/notification.service';
 
-@Module({})
+@Module({
+  providers: [NotificationService],
+})
 export class NotificationModule {}
