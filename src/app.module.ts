c749a5a5c091fcfa1a854dbc3dddd203c9d5f809
LeeSeokHo <rrgks@naver.com>
2023-06-06 18:01:39 +0900

    delete: delete appModule providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { apiModules } from '@src/apis';
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
 import { CoreModule } from '@src/core/core.module';
 import { LoggerMiddleware } from '@src/middlewares/logger.middleware';
 
 @Module({
   imports: [CoreModule, ...apiModules],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
