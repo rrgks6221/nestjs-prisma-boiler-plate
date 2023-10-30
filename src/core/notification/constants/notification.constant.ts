56fd99e9811e44baa4be4710f920b8d1b07b7b0b
LeeSeokHo <rrgks@naver.com>
2023-10-30 18:33:55 +0900

    modify: token 문자열에서 심볼로 변경

--- src/core/notification/constants/notification.constant.ts
+++ src/core/notification/constants/notification.constant.ts
@@ -1,5 +1,5 @@
 // discord.js 라이브러리 관련 토큰
-export const EMBED_BUILDER_TOKEN = 'EMBED_BUILDER_TOKEN';
+export const EMBED_BUILDER_TOKEN = Symbol('EMBED_BUILDER_TOKEN');
 
 // discord.js 라이브러리 관련 토큰
-export const WEBHOOK_CLIENT_TOKEN = 'WEBHOOK_CLIENT_TOKEN';
+export const WEBHOOK_CLIENT_TOKEN = Symbol('WEBHOOK_CLIENT_TOKEN');
