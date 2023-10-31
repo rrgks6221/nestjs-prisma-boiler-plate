74a53419099b7e9f7f0fd7b6238def7def561749
LeeSeokHo <rrgks@naver.com>
2023-10-31 18:27:42 +0900

    refactor: apiModule

--- src/apis/api.module.ts
+++ src/apis/api.module.ts
@@ -1,6 +1,10 @@
+import { Module } from '@nestjs/common';
 import { AuthModule } from '@src/apis/auth/auth.module';
 import { HealthModule } from '@src/apis/health/health.module';
 import { PostsModule } from '@src/apis/posts/posts.module';
 import { UsersModule } from '@src/apis/users/users.module';
 
-export const apiModules = [HealthModule, AuthModule, UsersModule, PostsModule];
+@Module({
+  imports: [HealthModule, AuthModule, UsersModule, PostsModule],
+})
+export class ApiModules {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { apiModules } from '@src/apis';
+import { ApiModules } from '@src/apis/api.module';
 import { CoreModule } from '@src/core/core.module';
 import { SuccessInterceptorModule } from '@src/interceptors/success-interceptor/success-interceptor.module';
 import { LoggerMiddleware } from '@src/middlewares/logger.middleware';
 
 @Module({
-  imports: [CoreModule, ...apiModules, SuccessInterceptorModule],
+  imports: [CoreModule, ApiModules, SuccessInterceptorModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
